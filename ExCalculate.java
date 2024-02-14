72778b1056bdcc0fa8ef17164e3e1a2057682923
Ramya B <01fe22bca202@kletech.ac.in>
2024-02-14 15:15:27 +0530

    adding two files for extended Calculator Branch

---
+++ ExCalculate.java
@@ -0,0 +1,9 @@
+public class ExCalculate {
+    public static double areaTri(double b, double h) {
+        return 0.5*b*h;
+    }
+    public static double modulus(double n1, double n2) {
+            return n1%n2;
+    }
+
+}
